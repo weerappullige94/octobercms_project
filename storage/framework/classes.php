2e3da0cbd0d7e9653b5f735657e59e75a79c90ed
Tharanga Weerappullige <tharangasadamali94@gmail.com>
2019-08-28 10:39:12 +0530

    changing file

--- storage/framework/classes.php
+++ storage/framework/classes.php
@@ -2,4 +2,6 @@
   'october\\demo\\Plugin' => 'plugins\\october\\demo\\Plugin.php',
   'backend\\Controllers\\index' => 'modules\\backend\\controllers\\index.php',
   'backend\\Controllers\\auth' => 'modules\\backend\\controllers\\auth.php',
+  'system\\Controllers\\settings' => 'modules\\system\\controllers\\settings.php',
+  'cms\\Controllers\\index' => 'modules\\cms\\controllers\\index.php',
 );
